bd46bf87fe2b4388653d160a66d97db9573b0d29
Tony Rasa <trasa@meancat.com>
2022-01-25 11:06:56 -0800

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.listen(8000, function() { console.log("server is running"); });
+app.use(bodyParser.json());
+
 
 const mockUserData = [ {name:'Mark'}, {name:'Jill'}];
 
@@ -21,3 +23,26 @@ app.get('/users/:id', (req, res) => {
         user: req.params.id
     });
 });    
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const mockUserName = "billythekid";
+    const mockPassword = "supersecret";
+
+    if (username === mockUserName && password == mockPassword) {
+        res.json({
+            success: true,
+            message: "password and username match!",
+            token: "encrypted token goes here"
+        });
+    } else {
+        res.json({
+            success: false,
+            message: "password and username do not match"
+        });
+    }
+});
+
+app.listen(8000, function() { console.log("server is running"); });
+
