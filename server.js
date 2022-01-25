e6cf70e65f5667431965f60e45be4ae7ed8ad735
Tony Rasa <trasa@meancat.com>
2022-01-25 10:55:56 -0800

    add dynamic GET route

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.get('/users', (req, res) => {
         users: mockUserData
     });
 });    
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: "got one user",
+        user: req.params.id
+    });
+});    
