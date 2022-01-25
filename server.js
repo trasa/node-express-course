5431d89532642bf4f78cd5c4a4299153fee64821
Tony Rasa <trasa@meancat.com>
2022-01-25 10:51:34 -0800

    add first get route

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const app = express();
 
 app.listen(8000, function() { console.log("server is running"); });
+
+const mockUserData = [ {name:'Mark'}, {name:'Jill'}];
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: 'successfully got users, nice!',
+        users: mockUserData
+    });
+});    
